b9c860a329a0b462ef4c958a7f44efacedf73134
Kramer Campbell <kramer@kramerc.com>
2026-02-19 22:54:12 -0800

    Reimplement IPC for Windows

--- src/discord/ipc/windows.rs
+++ src/discord/ipc/windows.rs
@@ -1,7 +1,82 @@
 use std::error::Error;
 
-use crate::discord::ipc::Client;
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt},
+    net::windows::named_pipe::ClientOptions,
+};
+use tokio_util::bytes::BytesMut;
+
+use crate::discord::{self, ipc::{Client, Data, OpCode}};
 
 pub async fn connect(client: &Client) -> Result<(), Box<dyn Error>> {
-  todo!()
+    let path = discord::ipc::path(&client.name);
+    let pipe = ClientOptions::new().open(&path)?;
+
+    let (mut read_half, mut write_half) = tokio::io::split(pipe);
+    let mut discord_rx = client.channel.1.resubscribe();
+
+    // Writer: Switch -> Discord IPC
+    let name = client.name.clone();
+    tokio::spawn(async move {
+        loop {
+            let data = match discord_rx.recv().await {
+                Ok(data) => data,
+                Err(e) => {
+                    // Channel is closed, closing writer
+                    tracing::error!("Error reading from Discord broadcast channel: {}", e);
+                    break;
+                }
+            };
+
+            tracing::trace!("Switch -> Discord IPC: {} {}", name, data.msg);
+
+            let mut buf = data.to_buf();
+            write_half.write_all_buf(&mut buf).await?;
+        }
+
+        Ok::<_, io::Error>(())
+    });
+
+    // Reader: Discord IPC -> Switch
+    let name = client.name.clone();
+    let switch_tx = client.switch_tx.clone();
+    tokio::spawn(async move {
+        loop {
+            // Expect a u32 indicating opcode
+            let opcode = OpCode::from_u32(read_half.read_u32_le().await?)?;
+
+            // Expect a u32 indicating length
+            let length = read_half.read_u32_le().await? as usize;
+
+            // Read the message
+            let mut buf = BytesMut::with_capacity(length);
+            let n = read_half.read_buf(&mut buf).await?;
+
+            if n == 0 {
+                // Client disconnected
+                tracing::info!("{} disconnected", name);
+                break;
+            }
+
+            match String::from_utf8(buf.to_vec()) {
+                Ok(msg) => {
+                    tracing::trace!("Discord IPC Read: {} {} {} {}", name, opcode, length, msg);
+
+                    let data = Data {
+                        opcode,
+                        msg
+                    };
+
+                    if let Err(e) = switch_tx.send(data) {
+                        tracing::error!("Could not send to mpsc channel: {}", e);
+                    }
+                },
+                Err(e) => tracing::error!("Could not decode message to UTF-8 string: {}", e),
+            }
+        }
+
+        Ok::<_, io::Error>(())
+    });
+
+    Ok(())
 }
--- src/switch/ipc/windows.rs
+++ src/switch/ipc/windows.rs
@@ -1,7 +1,117 @@
 use std::error::Error;
 
-use crate::switch::ipc::Server;
+use tokio::{
+    io::{self, AsyncReadExt, AsyncWriteExt},
+    net::windows::named_pipe::ServerOptions,
+    sync::mpsc,
+};
+use tokio_util::bytes::BytesMut;
+
+use crate::{discord::ipc::{Data, OpCode}, switch::ipc::{Client, Server}};
+
+async fn handle(server: Server, pipe: tokio::net::windows::named_pipe::NamedPipeServer) -> Result<(), Box<dyn Error>> {
+    tracing::info!("Received new client");
+
+    let (mut read_half, mut write_half) = tokio::io::split(pipe);
+    let (tx, mut rx) = mpsc::unbounded_channel::<Data>();
+    let mut client = Client::new(server, tx);
+
+    // Writer
+    tokio::spawn(async move {
+        loop {
+            let data = match rx.recv().await {
+                Some(msg) => msg,
+                None => {
+                    // Channel is closed, closing writer
+                    break;
+                }
+            };
+
+            tracing::trace!("Switch Writer: {}", data.msg);
+
+            let mut buf = data.to_buf();
+            write_half.write_all_buf(&mut buf).await?;
+        }
+
+        Ok::<_, io::Error>(())
+    });
+
+    // Reader
+    loop {
+        if client.closed {
+            break;
+        }
+
+        // Expect a u32 indicating opcode
+        let opcode = OpCode::from_u32(read_half.read_u32_le().await?)?;
+
+        // Expect a u32 indicating length
+        let length = read_half.read_u32_le().await? as usize;
+
+        // Read the message
+        let mut buf = BytesMut::with_capacity(length);
+        let n = read_half.read_buf(&mut buf).await?;
+
+        if n == 0 {
+            break;
+        }
+
+        let msg = String::from_utf8(buf.to_vec())?;
+        tracing::trace!("Switch Reader: {}", msg);
+
+        let data = Data {
+            opcode,
+            msg
+        };
+
+        client.handle(data).await?;
+    }
+
+    client.disconnected();
+
+    Ok(())
+}
 
 pub async fn start(server: Server) -> Result<(), Box<dyn Error>> {
-    todo!()
+    let path = server.path();
+
+    let mut pipe_server = ServerOptions::new()
+        .first_pipe_instance(true)
+        .create(&path)?;
+
+    tracing::info!("Server listening for clients");
+
+    loop {
+        let server = server.clone();
+
+        tokio::select! {
+            result = pipe_server.connect() => {
+                match result {
+                    Ok(()) => {
+                        let connected_pipe = pipe_server;
+
+                        // Create a new pipe instance for the next client
+                        pipe_server = ServerOptions::new().create(&path)?;
+
+                        tokio::spawn(async move {
+                            if let Err(e) = handle(server, connected_pipe).await {
+                                tracing::error!("Error handling client: {}", e);
+                            }
+                        });
+                    }
+                    Err(e) => {
+                        tracing::error!("Error accepting client: {}", e);
+                    }
+                }
+            }
+
+            _ = server.token.cancelled() => {
+                tracing::info!("Cancellation token triggered, stopping server");
+                break;
+            }
+        }
+    }
+
+    tracing::info!("Server closed");
+    Ok(())
 }
